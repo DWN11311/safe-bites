75b6204757b9103f0612b350d45d0bf9cbb5b2e4
WA7iD <rahmamohammed117@gmail.com>
2025-02-19 01:45:09 +0200

    add features component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FeaturesComponent } from './components/features/features.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FeaturesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
