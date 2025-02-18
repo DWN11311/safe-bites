9652f86eef34755d6f4f8f06accc5952993c6599
DWN11311 <mohamedkhatab2002@gmail.com>
2025-02-18 07:54:18 +0200

    feat: add life-style and quote components with styles and templates

---
+++ src/app/components/life-style/life-style.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-life-style',
+  imports: [],
+  templateUrl: './life-style.component.html',
+  styleUrl: './life-style.component.css'
+})
+export class LifeStyleComponent {
+
+}
