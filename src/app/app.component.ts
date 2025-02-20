cdb93776447cb217caed037956cb7c108b48c526
radwa elsayed <radwa elsayed@gmail.com>
2025-02-20 15:41:05 +0200

    feat: header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LifeStylesComponent } from "./components/life-styles/life-styles.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ LifeStylesComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
