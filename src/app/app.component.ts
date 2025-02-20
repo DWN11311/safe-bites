1218678efc8b0f56c0dc3123366b481a1b18179a
Asm-123 <asmaa2470a@gmail.com>
2025-02-20 15:39:17 +0200

    reviews and about us

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { LifeStyleComponent } from './components/life-style/life-style.component';
+import { CustomerReviewsComponent } from './customer-reviews/customer-reviews.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AboutUsComponent,CustomerReviewsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
