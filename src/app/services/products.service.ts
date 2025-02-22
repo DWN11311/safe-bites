2f41526a6541a4534d925b028b1579414262ab34
radwa elsayed <radwa elsayed@gmail.com>
2025-02-22 13:56:53 +0200

    feat: services

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -2,16 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
-  private readonly url = "http://localhost:3000/products"
-  constructor(private http: HttpClient) { }
+  private readonly url = 'http://localhost:3000/products';
+  constructor(private http: HttpClient) {}
   getAllProducts() {
-    return this.http.get(this.url)
+    return this.http.get(this.url);
   }
   getProductById(id: number) {
-    return this.http.get(this.url + "/" + id)
+    return this.http.get(this.url + '/' + id);
   }
   // createProduct(porduct:Product){
   //   return this.http.post(this.url, porduct);
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -2,16 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
-  private readonly url = "http://localhost:3000/users"
-  constructor(private http: HttpClient) { }
+  private readonly url = 'http://localhost:3000/users';
+  constructor(private http: HttpClient) {}
   getAllUsers() {
-    return this.http.get(this.url)
+    return this.http.get(this.url);
   }
   getUserById(id: number) {
-    return this.http.get(this.url + "/" + id)
+    return this.http.get(this.url + '/' + id);
   }
   getUserByEmail(email: string) {
     return this.http.get(`${this.url}?email=${email}`);
