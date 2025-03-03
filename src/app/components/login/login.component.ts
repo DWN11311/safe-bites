4e25fc47a6e5963a96bc26d5c392e9f2086a5394
Asm-123 <asmaa2470a@gmail.com>
2025-03-03 15:23:32 +0200

    token

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-login',
@@ -9,4 +10,22 @@ import { RouterModule } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  email:string="";
+  password:string="";
+  errorMessage: string="";
+  constructor( private UsersService: UsersService, private  router: Router){ }
+  login(){
+    this.UsersService.login(this.email,this.password).subscribe({
+      next:(data)=>{
+        console.log(data)
+        localStorage.setItem("token",data.token)
+        this.router.navigate(['/home'])
+      },
+      error: (err) => {
+        this.errorMessage = 'Login failed. Please check your credentials.';
+        console.error('Login error', err);
+      }
+    })
+  }
+}
