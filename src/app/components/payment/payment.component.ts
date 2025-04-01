49fab8d5d2683945e19d738498e1ee2882284565
Mohamed Ayman <mohamedkhatab2002@gmail.com>
2025-04-01 13:40:59 +0200

    resolve conflict

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -11,56 +11,63 @@ import {
   selector: 'app-payment',
   imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './payment.component.html',
-  styleUrl: './payment.component.css'
+  styleUrl: './payment.component.css',
 })
 export class PaymentComponent {
+  contactForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      phone: ['', [Validators.required, Validators.pattern('^01[0-9]{9}$')]],
+      FirstName: ['', [Validators.required]],
+      LastName: ['', [Validators.required]],
+      Street: ['', [Validators.required]],
+      City: ['', [Validators.required]],
+      Email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$'
+          ),
+        ],
+      ],
+    });
+  }
 
-   contactForm: FormGroup;
-    constructor(private fb: FormBuilder) {
-      this.contactForm = this.fb.group({
-        phone: ['', [Validators.required, Validators.pattern('^01[0-9]{9}$')]],
-        FirstName: ['', [Validators.required]],
-        LastName: ['', [Validators.required]],
-        Street: ['', [Validators.required]],
-        City: ['', [Validators.required]],
-        Email: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$')]],
-      });
-    }
-  
-    get phone() {
-      return this.contactForm.get('phone');
-    }
-  
+  get phone() {
+    return this.contactForm.get('phone');
+  }
+
   get FirstName() {
     return this.contactForm.get('FirstName');
-    }
+  }
   get LastName() {
     return this.contactForm.get('LastName');
-    }
+  }
 
-  get Street(){
+  get Street() {
     return this.contactForm.get('Street');
   }
-  
-  get City(){
+
+  get City() {
     return this.contactForm.get('City');
   }
 
-   get Email(){
-     return this.contactForm.get('Email');
-   }
-    submitForm() {
-      if (this.contactForm.invalid) {
-        this.contactForm.markAllAsTouched();
-        return;
-      }
-      console.log('Form Submitted:', this.contactForm.value);
+  get Email() {
+    return this.contactForm.get('Email');
+  }
+  submitForm() {
+    if (this.contactForm.invalid) {
+      this.contactForm.markAllAsTouched();
+      return;
     }
-  
-    checkInputValue(field: string) {
-      const control = this.contactForm.get(field);
-      if (control?.value) {
-        control.markAsTouched();
-      }
+    console.log('Form Submitted:', this.contactForm.value);
+  }
+
+  checkInputValue(field: string) {
+    const control = this.contactForm.get(field);
+    if (control?.value) {
+      control.markAsTouched();
     }
+  }
 }
