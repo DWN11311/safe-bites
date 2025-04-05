c197882278f003767abdeca24fa1b8efed1f8529
radwa elsayed <radwa elsayed@gmail.com>
2025-04-05 06:34:59 +0200

    scroll and fix routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Component, OnInit} from '@angular/core';
+import { Router, RouterOutlet ,NavigationEnd } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductsService } from './services/products.service';
 import { ReactiveFormsModule } from '@angular/forms';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +18,7 @@ import { ReactiveFormsModule } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   hiddenRoutes = ['/login', '/sign-up'];
   title = 'safe-bites';
 
@@ -25,5 +26,14 @@ export class AppComponent {
 
   shouldDisplayLayout(): boolean {
     return !this.hiddenRoutes.includes(this.router.url);
+  }
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd)
+      )
+      .subscribe(() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      });
   }
 }
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+import { CategoriesService } from '../../services/categories.service';
+import { Category } from '../../models/category.model';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterModule,CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  firstName: string | null = '';
+  categories: Category[] = [];
+ constructor(
+    private router: Router,
+    private categoriesService: CategoriesService
+  ) {}
+
+  ngOnInit() {
+  
+    this.firstName = localStorage.getItem('firstName');
+    this.categoriesService.getCategories().subscribe(data => {
+      this.categories = data as Category[];
+    });
+  }
+
+
+}
